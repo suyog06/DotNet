8099d27f50a4dbca6fbb7fd3cfc5d0c5119716c6
suyog06 <suyogshelar06@gmail.com>
2020-05-07 17:28:47 +0530

    Adding Files

---
+++ Level 1/Enums/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+public class Enums
+{
+    public static void Main()
+    {
+        Customer[] customers = new Customer[3];
+        customers[0] = new Customer()
+        {
+            Name = "Mark",
+            Gender = Gender.Male
+        };
+        customers[1] = new Customer()
+        {
+            Name = "Mary",
+            Gender = Gender.Female
+        };
+        customers[2] = new Customer()
+        {
+            Name = "Sam",
+            Gender = Gender.Unknown
+        };
+        foreach (Customer customer in customers)
+        {
+            Console.WriteLine("Name = {0} && Gender = {1}", customer.Name, GetGender(customer.Gender));
+        }
+    }
+
+
+    public static string GetGender(Gender gender)
+    {
+        switch (gender)
+        {
+            case Gender.Unknown:
+                return "Unknown";
+            case Gender.Male:
+                return "Male";
+            case Gender.Female:
+                return "Female";
+            default:
+                return "Invalid Data for Gender";
+        }
+    }
+}
+
+
+public enum Gender
+{
+    Unknown = 0,
+    Male = 1,
+    Female = 2
+}
+
+
+public class Customer
+{
+    public string Name { get; set; }
+    public Gender Gender { get; set; }
+}
